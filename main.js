a44204cf252c9cb8773f0c680255eaa286ced3cb
jcr <notavailable@notavailable.com>
2023-01-21 18:13:01 +0100

    Add clip-path and tooltip

--- main.js
+++ main.js
@@ -50,87 +50,118 @@ function showData(data) {
 
     xAxisGroup.call(xAxis.tickFormat(d3.timeFormat("%b %Y")))    
     yAxisGroup.call(yAxis.tickSizeInner(-netWidth)) // Add auxiliary horizontal lines
+        
+    // Add clipPath. Avoid displaying candles outside the chart area
+    const clip = svg.append("defs").append("clipPath").attr("id", "clip")
+    clip.append("rect")
+        .attr("width", netWidth)
+        .attr("height", netHeight)
+        .attr("x", 0)
+        .attr("y", 0)
     
-        // Show mouse lines, candlesticks and volume bars
-        const candleWidth = 0.45 * netWidth / data.length     
-        showCandles(data, candleWidth)
-        showVolume(data, candleWidth) 
-        showMouseLines()
-    }
+    elementGroup.attr("clip-path", "url(#clip)")
+    mouseLinesGroup.attr("clip-path", "url(#clip)")
     
-    function showCandles(data, candleWidth) {
-        const timeFormat = d3.timeFormat("%d-%m-%Y")
-        datesGroup.selectAll("g")
-            .data(data)
-            .enter().append("g")
-            .attr("class", "date")
-            .attr("id", d => timeFormat(d.date))
+    // Show mouse lines, candlesticks and volume bars
+    const candleWidth = 0.45 * netWidth / data.length     
+    showCandles(data, candleWidth)
+    showVolume(data, candleWidth) 
+    showMouseLines()
+}
 
-        datesGroup.selectAll("g.date").append("rect")
-            .attr("class", "backgroundBar")
-            .attr("x", d => x(d.date) - 1.25 * candleWidth / 2)
-            .attr("y", 0)
-            .attr("width", 1.25 * candleWidth)
-            .attr("height", height)
-            // .attr("fill", "black")
-            .attr("opacity", 0)
+function showCandles(data, candleWidth) {
+    const timeFormat = d3.timeFormat("%d-%m-%Y")
+    datesGroup.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "date")
+        .attr("id", d => timeFormat(d.date))
+        .on("mouseenter", d => showTooltip(d, d3.event.clientX, d3.event.clientY))
+        .on("mousemove", d => showTooltip(d, d3.event.clientX, d3.event.clientY))
+        .on("mouseleave", () => d3.select("div#tooltip").style("display", "none"))
     
-        const candleGroups = datesGroup.selectAll("g.date").append("g").attr("class", "candle")
-        // Candlestick wiskers
-        candleGroups.append("line")
-            .attr("class", d => d.open > d.close ? "redLine" : "greenLine")
-            .attr("x1", d => x(d.date))
-            .attr("y1", d => y(d.high))
-            .attr("x2", d => x(d.date))
-            .attr("y2", d => y(d.low))
-            
-        // Candlestick bodies
-        candleGroups.append("rect")
-            .attr("class", d => d.open > d.close ? "redCandle" : "greenCandle")
-            .attr("x", d => x(d.date) - candleWidth / 2)
-            .attr("y", d => y(Math.max(d.open, d.close)))
-            .attr("width", candleWidth)
-            .attr("height", d => Math.abs(y(d.open) - y(d.close)))
-    }
+    datesGroup.selectAll("g.date").append("rect")
+        .attr("class", "backgroundBar")
+        .attr("x", d => x(d.date) - 1.25 * candleWidth / 2)
+        .attr("y", 0)
+        .attr("width", 1.25 * candleWidth)
+        .attr("height", height)
+        // .attr("fill", "black")
+        .attr("opacity", 0)
+
+    const candleGroups = datesGroup.selectAll("g.date").append("g").attr("class", "candle")
+    // Candlestick wiskers
+    candleGroups.append("line")
+        .attr("class", d => d.open > d.close ? "redLine" : "greenLine")
+        .attr("x1", d => x(d.date))
+        .attr("y1", d => y(d.high))
+        .attr("x2", d => x(d.date))
+        .attr("y2", d => y(d.low))
+        
+    // Candlestick bodies
+    candleGroups.append("rect")
+        .attr("class", d => d.open > d.close ? "redCandle" : "greenCandle")
+        .attr("x", d => x(d.date) - candleWidth / 2)
+        .attr("y", d => y(Math.max(d.open, d.close)))
+        .attr("width", candleWidth)
+        .attr("height", d => Math.abs(y(d.open) - y(d.close)))
+}
+
+function showVolume(data, candleWidth) {
+    const volumeGroups = datesGroup.selectAll("g.date").append("g").attr("class", "volumeBar")
+    volumeGroups.append("rect")
+        .attr("class", d => d.open > d.close ? "redBar" : "greenBar")
+        .attr("x", d => x(d.date) - candleWidth / 2)
+        .attr("y", d => z(d.volume))
+        .attr("width", candleWidth)
+        .attr("height", d => netHeight- z(d.volume))
+}
+
+function showMouseLines() {
+    // Horizontal line
+    mouseLinesGroup.append("line")
+        .attr("id", "hMouseLine")
+        .attr("class", "mouseLine")
+        .attr("x1", 0)
+        .attr("x2", netWidth)
     
-    function showVolume(data, candleWidth) {
-        const volumeGroups = datesGroup.selectAll("g.date").append("g").attr("class", "volumeBar")
-        volumeGroups.append("rect")
-            .attr("class", d => d.open > d.close ? "redBar" : "greenBar")
-            .attr("x", d => x(d.date) - candleWidth / 2)
-            .attr("y", d => z(d.volume))
-            .attr("width", candleWidth)
-            .attr("height", d => netHeight- z(d.volume))
-    }
+    // Vertical line
+    mouseLinesGroup.append("line")
+        .attr("id", "vMouseLine")
+        .attr("class", "mouseLine")
+        .attr("y1", 0)
+        .attr("y2", netHeight)  
     
-    function showMouseLines() {
-        // Horizontal line
-        mouseLinesGroup.append("line")
-            .attr("id", "hMouseLine")
-            .attr("class", "mouseLine")
-            .attr("x1", 0)
-            .attr("x2", netWidth)
-        
-        // Vertical line
-        mouseLinesGroup.append("line")
-            .attr("id", "vMouseLine")
-            .attr("class", "mouseLine")
-            .attr("y1", 0)
-            .attr("y2", netHeight)  
-        
-        svg.on("mouseenter", () => moveMouseLines(d3.event.pageX, d3.event.pageY))
-            .on("mousemove", () => moveMouseLines(d3.event.pageX, d3.event.pageY))
-            .on("mouseleave", () => mouseLinesGroup.style("display", "none"))
-    }
+    svg.on("mouseenter", () => moveMouseLines(d3.event.pageX, d3.event.pageY))
+        .on("mousemove", () => moveMouseLines(d3.event.pageX, d3.event.pageY))
+        .on("mouseleave", () => mouseLinesGroup.style("display", "none"))
+}
+
+function moveMouseLines(x, y) {
+    const mouseOffsetY = margin.top + 7
+    const hMouseLine = d3.select("line#hMouseLine")
+    hMouseLine.attr("y1", y - mouseOffsetY).attr("y2", y - mouseOffsetY)
     
-    function moveMouseLines(x, y) {
-        const mouseOffsetY = margin.top + 7
-        const hMouseLine = d3.select("line#hMouseLine")
-        hMouseLine.attr("y1", y - mouseOffsetY).attr("y2", y - mouseOffsetY)
-        
-        const mouseOffsetX = margin.left + 7
-        const vMouseLine = d3.select("line#vMouseLine")
-        vMouseLine.attr("x1", x - mouseOffsetX).attr("x2", x - mouseOffsetX).style("display", "block")
-        
-        mouseLinesGroup.style("display", "block")
-    }
+    const mouseOffsetX = margin.left + 7
+    const vMouseLine = d3.select("line#vMouseLine")
+    vMouseLine.attr("x1", x - mouseOffsetX).attr("x2", x - mouseOffsetX).style("display", "block")
+    
+    mouseLinesGroup.style("display", "block")
+}
+
+function showTooltip(d, x, y) {
+    const timeFormat = d3.timeFormat("%-d %B %Y")
+    d3.select("div#tooltip")
+        .style("display", "block")
+        .style("left", `${x + 15}px`)
+        .style("top", `${y + 30}px`) 
+
+    d3.select("td#date").text(`${timeFormat(d.date)}`)
+    d3.select("td#open").text(`${d.open.toFixed(2)}`)
+    d3.select("td#high").text(`${d.high.toFixed(2)}`)
+    d3.select("td#low").text(`${d.low.toFixed(2)}`)
+    d3.select("td#close").text(`${d.close.toFixed(2)}`)
+    d3.select("td#change").attr("class", d.open > d.close ? "redText" : "greenText").text(`${(d.close - d.open).toFixed(2)}`)
+    d3.select("td#percentageChange").attr("class",  d.open > d.close ? "redText" : "greenText").text(`${((d.close / d.open - 1) * 100).toFixed(2)}%`)
+    d3.select("td#volume").text(`€${(d.volume / 10**6).toFixed(2)} M`)
+}
