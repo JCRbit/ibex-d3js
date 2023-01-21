52d2eda4034431f8489103f4e804e538ee109256
jcr <notavailable@notavailable.com>
2023-01-21 18:02:13 +0100

    Add minor changes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const netHeight = height - margin.bottom - margin.top
 const svg = d3.select("div#chart").append("svg").attr("width", width).attr("height", height)
 const elementGroup = svg.append("g").attr("id", "elementGroup").attr("transform", `translate(${margin.left}, ${margin.top})`)
 const datesGroup = elementGroup.append("g").attr("id", "datesGroup")
-const mouseLinesGroup = elementGroup.append("g").attr("id", "mouseLinesGroup")
+const mouseLinesGroup = svg.append("g").attr("id", "mouseLinesGroup").attr("transform", `translate(${margin.left}, ${margin.top})`)
 const axisGroup = svg.append("g").attr("id", "axisGroup")
 const xAxisGroup = axisGroup.append("g").attr("id", "xAxisGroup").attr("transform", `translate(${margin.left}, ${height - margin.bottom})`)
 const yAxisGroup = axisGroup.append("g").attr("id", "yAxisGroup").attr("transform", `translate(${margin.left}, ${margin.top})`)
@@ -49,87 +49,88 @@ function showData(data) {
     z.domain(d3.extent(data, d => d.volume))
 
     xAxisGroup.call(xAxis.tickFormat(d3.timeFormat("%b %Y")))    
-    yAxisGroup.call(yAxis.tickSizeInner(-netWidth))
-
-    // Show mouse lines, candlesticks and volume bars
-    const candleWidth = 0.45 * netWidth / data.length     
-    showMouseLines()
-    showCandles(data, candleWidth)
-    showVolume(data, candleWidth) 
-}
-
-function showCandles(data, candleWidth) {
-    const timeFormat = d3.timeFormat("%d-%m-%Y")
-    datesGroup.selectAll("g")
-        .data(data)
-        .enter().append("g")
-        .attr("id", d => timeFormat(d.date))
+    yAxisGroup.call(yAxis.tickSizeInner(-netWidth)) // Add auxiliary horizontal lines
     
-    datesGroup.selectAll("g").append("rect")
-        .attr("x", d => x(d.date) - 1.25 * candleWidth / 2)
-        .attr("y", 0)
-        .attr("width", 1.25 * candleWidth)
-        .attr("height", height)
-        // .attr("fill", "black")
-        .attr("opacity", 0) 
+        // Show mouse lines, candlesticks and volume bars
+        const candleWidth = 0.45 * netWidth / data.length     
+        showCandles(data, candleWidth)
+        showVolume(data, candleWidth) 
+        showMouseLines()
+    }
+    
+    function showCandles(data, candleWidth) {
+        const timeFormat = d3.timeFormat("%d-%m-%Y")
+        datesGroup.selectAll("g")
+            .data(data)
+            .enter().append("g")
+            .attr("class", "date")
+            .attr("id", d => timeFormat(d.date))
+
+        datesGroup.selectAll("g.date").append("rect")
+            .attr("class", "backgroundBar")
+            .attr("x", d => x(d.date) - 1.25 * candleWidth / 2)
+            .attr("y", 0)
+            .attr("width", 1.25 * candleWidth)
+            .attr("height", height)
+            // .attr("fill", "black")
+            .attr("opacity", 0)
+    
+        const candleGroups = datesGroup.selectAll("g.date").append("g").attr("class", "candle")
+        // Candlestick wiskers
+        candleGroups.append("line")
+            .attr("class", d => d.open > d.close ? "redLine" : "greenLine")
+            .attr("x1", d => x(d.date))
+            .attr("y1", d => y(d.high))
+            .attr("x2", d => x(d.date))
+            .attr("y2", d => y(d.low))
+            
+        // Candlestick bodies
+        candleGroups.append("rect")
+            .attr("class", d => d.open > d.close ? "redCandle" : "greenCandle")
+            .attr("x", d => x(d.date) - candleWidth / 2)
+            .attr("y", d => y(Math.max(d.open, d.close)))
+            .attr("width", candleWidth)
+            .attr("height", d => Math.abs(y(d.open) - y(d.close)))
+    }
+    
+    function showVolume(data, candleWidth) {
+        const volumeGroups = datesGroup.selectAll("g.date").append("g").attr("class", "volumeBar")
+        volumeGroups.append("rect")
+            .attr("class", d => d.open > d.close ? "redBar" : "greenBar")
+            .attr("x", d => x(d.date) - candleWidth / 2)
+            .attr("y", d => z(d.volume))
+            .attr("width", candleWidth)
+            .attr("height", d => netHeight- z(d.volume))
+    }
+    
+    function showMouseLines() {
+        // Horizontal line
+        mouseLinesGroup.append("line")
+            .attr("id", "hMouseLine")
+            .attr("class", "mouseLine")
+            .attr("x1", 0)
+            .attr("x2", netWidth)
         
-    const candleGroups = datesGroup.selectAll("g").append("g").attr("class", "candle")
-    // Candlestick wiskers
-    candleGroups.append("line")
-        .attr("class", d => d.open > d.close ? "redLine" : "greenLine")
-        .attr("x1", d => x(d.date))
-        .attr("y1", d => y(d.high))
-        .attr("x2", d => x(d.date))
-        .attr("y2", d => y(d.low))
+        // Vertical line
+        mouseLinesGroup.append("line")
+            .attr("id", "vMouseLine")
+            .attr("class", "mouseLine")
+            .attr("y1", 0)
+            .attr("y2", netHeight)  
         
-    // Candlestick bodies
-    candleGroups.append("rect")
-        .attr("class", d => d.open > d.close ? "redCandle" : "greenCandle")
-        .attr("x", d => x(d.date) - candleWidth / 2)
-        .attr("y", d => y(Math.max(d.open, d.close)))
-        .attr("width", candleWidth)
-        .attr("height", d => Math.abs(y(d.open) - y(d.close)))
-}
+        svg.on("mouseenter", () => moveMouseLines(d3.event.pageX, d3.event.pageY))
+            .on("mousemove", () => moveMouseLines(d3.event.pageX, d3.event.pageY))
+            .on("mouseleave", () => mouseLinesGroup.style("display", "none"))
+    }
     
-function showVolume(data, candleWidth) {
-    const volumeGroups = datesGroup.selectAll("g").append("g").attr("class", "volumeBar")
-    volumeGroups.append("rect")
-        .attr("class", d => d.open > d.close ? "redBar" : "greenBar")
-        .attr("x", d => x(d.date) - candleWidth / 2)
-        .attr("y", d => z(d.volume))
-        .attr("width", candleWidth)
-        .attr("height", d => netHeight- z(d.volume))
-}
-
-function showMouseLines() {
-    // Horizontal line
-    mouseLinesGroup.append("line")
-        .attr("id", "hMouseLine")
-        .attr("class", "mouseLine")
-        .attr("x1", 0)
-        .attr("x2", width)
-    
-    // Vertical line
-    mouseLinesGroup.append("line")
-        .attr("id", "vMouseLine")
-        .attr("class", "mouseLine")
-        .attr("y1", 0)
-        .attr("y2", height)  
-    
-    svg.on("mouseenter", () => mouseLinesGroup.style("display", "block"))
-        .on("mousemove", () => moveMouseLines(d3.event.clientX, d3.event.clientY))
-        .on("mouseleave", () => mouseLinesGroup.style("display", "none"))
-}
-
-function moveMouseLines(x, y) {
-    const mouseOffsetY = 8
-    const verticalMargins = margin.top + mouseOffsetY 
-    const hMouseLine = d3.select("line#hMouseLine")
-    hMouseLine.attr("y1", y - verticalMargins).attr("y2", y - verticalMargins).style("display", "block")
-    
-    const mouseOffsetX = 8
-    const horizontalMargins = margin.left + mouseOffsetX 
-    const vMouseLine = d3.select("line#vMouseLine")
-    vMouseLine.attr("x1", x - horizontalMargins).attr("x2", x - horizontalMargins).style("display", "block")
-}
-
+    function moveMouseLines(x, y) {
+        const mouseOffsetY = margin.top + 7
+        const hMouseLine = d3.select("line#hMouseLine")
+        hMouseLine.attr("y1", y - mouseOffsetY).attr("y2", y - mouseOffsetY)
+        
+        const mouseOffsetX = margin.left + 7
+        const vMouseLine = d3.select("line#vMouseLine")
+        vMouseLine.attr("x1", x - mouseOffsetX).attr("x2", x - mouseOffsetX).style("display", "block")
+        
+        mouseLinesGroup.style("display", "block")
+    }
